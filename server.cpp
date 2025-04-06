40198187e28e4bdd588ebc9b6f254fa4e78e6f1c
karolg02 <karolglanowski02@gmail.com>
2025-04-06 19:20:47 +0200

    +client sends message and server receives it

--- server.cpp
+++ server.cpp
@@ -1,5 +1,58 @@
 #include <iostream>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+
+static void die(const char *msg){
+    int error = errno;
+    fprintf(stderr, "[%d] %s\n", error, msg);
+    abort();
+}
+
+static void do_something(int connfd) {
+    char rbuf[64] = {};
+    ssize_t n = read(connfd, rbuf, sizeof(rbuf) - 1);
+    if (n < 0) {
+        die("read() error");
+    }
+    printf("client says: %s\n", rbuf);
+
+    char wbuf[] = "Received";
+    write(connfd, wbuf, strlen(wbuf));
+}
 
 int main() {
+    int fd = socket(AF_INET, SOCK_STREAM, 0);
+
+    //needed
+    int val = 1;
+    setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val));
+
+    //bind to ip
+    struct sockaddr_in addr = {};
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(8080);        //port
+    addr.sin_addr.s_addr = htonl(0);    //wildcard IP 0.0.0.0
+    int rv = bind(fd, (const struct sockaddr *)&addr, sizeof(addr));
+    if (rv) { die("bind()"); }
+
+    rv = listen(fd, SOMAXCONN);
+    if (rv) { die("listen()"); }
+
+    while (true) {
+        //accept
+        struct sockaddr_in client_addr = {};
+        socklen_t addrlen = sizeof(client_addr);
+        int connfd = accept(fd, (struct sockaddr *)&client_addr, &addrlen);
+        if (connfd < 0) {
+            continue;   //error
+        }
+
+        do_something(connfd);
+        close(connfd);
+    }
+
     return 1;
 }
