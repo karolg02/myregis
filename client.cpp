615325605b468b6dfb6f3c0c9910c1c6d32ae6fc
karolg02 <karolglanowski02@gmail.com>
2025-04-08 00:27:23 +0200

    +writing and reading stream of bytes

--- client.cpp
+++ client.cpp
@@ -1,13 +1,84 @@
+//g++ -Wall -Wextra -O2 -g 03_client.cpp -o client
+
 #include <iostream>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <string.h>
+#include <assert.h>
+
+const size_t k_max_msg = 4096;
 
 static void die(const char *msg){
     int error = errno;
     fprintf(stderr, "[%d] %s\n", error, msg);
     abort();
+}
+
+static void msg(const char *msg) {
+    fprintf(stderr, "%s\n", msg);
+}
+
+static int32_t read_full(int fd,char *buf, size_t n){
+    while(n>0){
+        ssize_t rv = read(fd, buf, n);
+        if(rv <= 0){
+            return -1;
+        }
+        assert((size_t)rv <= n);
+        n -= (size_t)rv;
+        buf += rv;
+    }
+    return 0;
+}
+
+static int32_t write_full(int fd, const char *buf, size_t n){
+    while(n>0){
+        ssize_t rv = write(fd,buf, n);
+        if(rv<=0){
+            return -1;
+        }
+        assert((size_t)rv <= n);
+        n -= (size_t)rv;
+        buf += rv;
+    }
+    return 0;
+}
+
+static int32_t query(int fd, const char *text) {
+    uint32_t len = (uint32_t)strlen(text);
+    if (len > k_max_msg) {
+        return -1;
+    }
+    // send request
+    char wbuf[4 + k_max_msg];
+    memcpy(wbuf, &len, 4);  // assume little endian
+    memcpy(&wbuf[4], text, len);
+    if (int32_t err = write_full(fd, wbuf, 4 + len)) {
+        return err;
+    }
+    // 4 bytes header
+    char rbuf[4 + k_max_msg + 1];
+    errno = 0;
+    int32_t err = read_full(fd, rbuf, 4);
+    if (err) {
+        msg(errno == 0 ? "EOF" : "read() error");
+        return err;
+    }
+    memcpy(&len, rbuf, 4);  // assume little endian
+    if (len > k_max_msg) {
+        msg("too long");
+        return -1;
+    }
+    // reply body
+    err = read_full(fd, &rbuf[4], len);
+    if (err) {
+        msg("read() error");
+        return err;
+    }
+    // do something
+    printf("server says: %.*s\n", len, &rbuf[4]);
+    return 0;
 }
 
 int main(){
@@ -25,15 +96,21 @@ int main(){
         die("connect");
     }
 
-    std::string msg;
-    std::cin >> msg;
-    write(fd, msg.c_str(), msg.length());
-
-    char rbuf[64] = {};
-    ssize_t n = read(fd, rbuf, sizeof(rbuf) - 1);
-    if (n < 0) {
-        die("read");
+    // multiple requests
+    int32_t err = query(fd, "hello1");
+    if (err) {
+        goto L_DONE;
     }
-    printf("server says: %s\n", rbuf);
+    err = query(fd, "hello2");
+    if (err) {
+        goto L_DONE;
+    }
+    err = query(fd, "hello3");
+    if (err) {
+        goto L_DONE;
+    }
+
+L_DONE:
     close(fd);
+    return 0;
 }
--- server.cpp
+++ server.cpp
@@ -1,3 +1,5 @@
+//g++ -Wall -Wextra -O2 -g 03_server.cpp -o server
+
 #include <iostream>
 #include <sys/socket.h>
 #include <arpa/inet.h>
@@ -6,10 +8,15 @@
 #include <string.h>
 #include <assert.h>
 
+
 static void die(const char *msg){
     int error = errno;
     fprintf(stderr, "[%d] %s\n", error, msg);
     abort();
+}
+
+static void msg(const char *msg) {
+    fprintf(stderr, "%s\n", msg);
 }
 
 static int32_t read_full(int fd,char *buf, size_t n){
@@ -19,13 +26,57 @@ static int32_t read_full(int fd,char *buf, size_t n){
             return -1;
         }
         assert((size_t)rv <= n);
+        n -= (size_t)rv;
         buf += rv;
     }
     return 0;
 }
 
-static int32_t one_request(int connfd){
+static int32_t write_full(int fd, const char *buf, size_t n){
+    while(n>0){
+        ssize_t rv = write(fd,buf, n);
+        if(rv<=0){
+            return -1;
+        }
+        assert((size_t)rv <= n);
+        n -= (size_t)rv;
+        buf += rv;
+    }
+    return 0;
+}
 
+const size_t k_max_msg = 4096;
+
+static int32_t one_request(int connfd) {
+    //4 bytes header
+    char rbuf[4 + k_max_msg];
+    errno = 0;
+    int32_t err = read_full(connfd, rbuf, 4);
+    if (err) {
+        msg(errno == 0 ? "EOF" : "read() error");
+        return err;
+    }
+    uint32_t len = 0;
+    memcpy(&len, rbuf, 4); //assume little endian
+    if (len > k_max_msg) {
+        msg("too long");
+        return -1;
+    }
+    //request body
+    err = read_full(connfd, &rbuf[4], len);
+    if (err) {
+        msg("read() error");
+        return err;
+    }
+    //do something
+    printf("client says: %.*s\n", len, &rbuf[4]);
+    //reply using the same protocol
+    const char reply[] = "world";
+    char wbuf[4 + sizeof(reply)];
+    len = (uint32_t)strlen(reply);
+    memcpy(wbuf, &len, 4);
+    memcpy(&wbuf[4], reply, len);
+    return write_full(connfd, wbuf, 4 + len);
 }
 
 static void do_something(int connfd) {
